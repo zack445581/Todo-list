55dd6891e31a26e55ef2fd01d47ba6815cf46cc5
Akhdari <zakaria.akhdari@flyer.be>
2024-01-23 17:46:11 +0100

    remove from list

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var input = document.getElementById("input-text");
 var btn_add = document.getElementById("add-to-list");
 var list = document.querySelector(".list");
+var remove = document.querySelector(".remove");
+var list_item = document.getElementsByClassName("list-item");
+var arr = []
 
 btn_add.addEventListener("click", function () {
   AddtoTodolist();
@@ -10,11 +13,31 @@ function AddtoTodolist() {
   if (input.value === "") {
     input.style.border = "1px solid red";
   } else {
-    input.style.border = "initial";
+    input.style.borderColor = "rgb(133, 133, 133)";
     var li = document.createElement("li");
     li.className = "list-item";
     list.appendChild(li);
-    li.textContent = input.value;
+    // li.textContent = input.value;
+
+    arr.push(input.value);
+    console.log(arr);
+    arr.map(item => li.textContent = item);
+
+    var remove = document.createElement("span");
+    remove.className = "remove";
+    remove.innerHTML = "&#10060;";
+    li.appendChild(remove);
   }
   input.value = "";
+
+  remove.addEventListener("click", function () {
+    RemovefromTodolist();
+  });
+}
+
+function RemovefromTodolist() {
+  // for (let i = 0; i < arr.length; i++) {
+  //   const element = arr.length;
+  //   console.log(element)
+  // }
 }
